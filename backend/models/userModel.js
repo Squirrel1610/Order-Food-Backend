4c940c86e359f44fcbe187172bce6c3fe51e7046
Squirrel1610 <thinh.nd.60cntt@ntu.edu.vn>
2021-12-26 14:37:18 +0700

    template email forgot password

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -2,6 +2,8 @@ const knex = require("../database/knex_db.js");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const nodemailer = require("nodemailer");
+const hbs = require("nodemailer-express-handlebars");
+const path = require("path");
 
 module.exports = {
   //đăng ký tài khoản
@@ -322,16 +324,64 @@ module.exports = {
         },
       });
 
+      const handlebarOptions = {
+        viewEngine: {
+          extName: ".html",
+          partialsDir: path.resolve("../views"),
+          defaultLayout: false,
+        },
+        viewPath: path.resolve("../views"),
+        extName: ".html",
+      };
+
+      transporter.use("compile", hbs(handlebarOptions));
+
       var mailOptions = {
         from: "slthinhtu2@gmail.com",
         to: email,
         subject: "Forgot Password",
-        html:
-          "<h1>Hi " +
-          count[0].hoten +
-          "</h1><p>We have sent you this email in response to your request to reset password</p><p>Here is your new password: " +
-          randPassword +
-          "</p><p>We recommend that you keep this password secure and not share it with anyone. If you feel this password has been compromised, you can change it by going to your profile and clicking on the 'Change Password' link.</p><h5>Pizza Food Customer Service</h5>",
+        attachments: [
+          {
+            filename: "animated_header.gif",
+            path: path.join(__dirname, "/images/animated_header.gif"),
+            cid: "animated_header",
+          },
+          {
+            filename: "bee.png",
+            path: path.join(__dirname, "/images/bee.png"),
+            cid: "bee",
+          },
+          {
+            filename: "body_background_2.png",
+            path: path.join(__dirname, "/images/body_background_2.png"),
+            cid: "body_background",
+          },
+          {
+            filename: "bottom_img.png",
+            path: path.join(__dirname, "/images/bottom_img.png"),
+            cid: "bottom",
+          },
+          {
+            filename: "instagram2x.png",
+            path: path.join(__dirname, "/images/instagram2x.png"),
+            cid: "instagram2x",
+          },
+          {
+            filename: "logo.jpg",
+            path: path.join(__dirname, "/images/logo.jpg"),
+            cid: "logo",
+          },
+          {
+            filename: "twitter2x.png",
+            path: path.join(__dirname, "/images/twitter2x.png"),
+            cid: "twitter2x",
+          },
+        ],
+        template: "index",
+        context: {
+          randPassword: randPassword,
+          hoten: count[0].hoten,
+        },
       };
 
       transporter.sendMail(mailOptions, function (error, info) {
@@ -395,16 +445,64 @@ module.exports = {
         },
       });
 
+      const handlebarOptions = {
+        viewEngine: {
+          extName: ".html",
+          partialsDir: path.resolve("../views"),
+          defaultLayout: false,
+        },
+        viewPath: path.resolve("../views"),
+        extName: ".html",
+      };
+
+      transporter.use("compile", hbs(handlebarOptions));
+
       var mailOptions = {
         from: "slthinhtu2@gmail.com",
         to: email,
         subject: "Forgot Password",
-        html:
-          "<h1>Hi " +
-          count[0].hoten +
-          "</h1><p>We have sent you this email in response to your request to reset password</p><p>Here is your new password: " +
-          randPassword +
-          "</p><p>We recommend that you keep this password secure and not share it with anyone. If you feel this password has been compromised, you can change it by going to your profile and clicking on the 'Change Password' link.</p><h5>Pizza Food Customer Service</h5>",
+        attachments: [
+          {
+            filename: "animated_header.gif",
+            path: path.join(__dirname, "/images/animated_header.gif"),
+            cid: "animated_header",
+          },
+          {
+            filename: "bee.png",
+            path: path.join(__dirname, "/images/bee.png"),
+            cid: "bee",
+          },
+          {
+            filename: "body_background_2.png",
+            path: path.join(__dirname, "/images/body_background_2.png"),
+            cid: "body_background",
+          },
+          {
+            filename: "bottom_img.png",
+            path: path.join(__dirname, "/images/bottom_img.png"),
+            cid: "bottom",
+          },
+          {
+            filename: "instagram2x.png",
+            path: path.join(__dirname, "/images/instagram2x.png"),
+            cid: "instagram2x",
+          },
+          {
+            filename: "logo.jpg",
+            path: path.join(__dirname, "/images/logo.jpg"),
+            cid: "logo",
+          },
+          {
+            filename: "twitter2x.png",
+            path: path.join(__dirname, "/images/twitter2x.png"),
+            cid: "twitter2x",
+          },
+        ],
+        template: "index",
+        context: {
+          randPassword: randPassword,
+          hoten: count[0].hoten,
+        },
       };
 
       transporter.sendMail(mailOptions, function (error, info) {
